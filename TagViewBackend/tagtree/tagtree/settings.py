b375bad0a6eb1229ee67e392c1e501f5ffc955e7
sameernarang1234 <samre1324@gmail.com>
2025-01-02 10:57:38 +0530

    database changed

--- TagViewBackend/tagtree/tagtree/settings.py
+++ TagViewBackend/tagtree/tagtree/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'tagtree.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TreeHierarchyDB',
+        'USER': 'sameernarang',
+        'PASSWORD': 'TreeHierarchy1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
