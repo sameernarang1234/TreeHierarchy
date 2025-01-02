1f7ac33482cec09ef9844c2dcdfec9f7a2e7a7ef
sameernarang1234 <samre1324@gmail.com>
2025-01-02 10:09:03 +0530

    python version changed for vercel

--- TagViewBackend/tagtree/build_files.sh
+++ TagViewBackend/tagtree/build_files.sh
@@ -1,13 +1,10 @@
 echo " BUILD START"
-echo "PYTHON VERSION"
-python --version
-echo "PYTHON 3 VERSION"
 python3 --version
 echo "PIP INSTALL STARTED"
-python3.9 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 echo "PIP INSTALL ENDED"
 echo "PACKAGES INSTALL STARTED"
-python3.9  -m pip install -r requirements.txt
+python3  -m pip install -r requirements.txt
 echo "PACKAGES INSTALL ENDED"
-python3.9 manage.py collectstatic  --noinput --clear
+python3 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
