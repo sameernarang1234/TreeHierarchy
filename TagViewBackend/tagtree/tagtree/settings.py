d6f27019e4379be8ee4b475ad83065317626ead6
sameernarang1234 <samre1324@gmail.com>
2025-01-02 09:34:25 +0530

    static files details added in backend

--- TagViewBackend/tagtree/tagtree/settings.py
+++ TagViewBackend/tagtree/tagtree/settings.py
@@ -129,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
