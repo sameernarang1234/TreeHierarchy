6d5b4bc2951fe8e87c21fbc159bc1f28d0c1ef52
sameernarang1234 <samre1324@gmail.com>
2025-01-02 10:05:50 +0530

    more logs added for vercel

--- TagViewBackend/tagtree/build_files.sh
+++ TagViewBackend/tagtree/build_files.sh
@@ -1,4 +1,13 @@
 echo " BUILD START"
+echo "PYTHON VERSION"
+python --version
+echo "PYTHON 3 VERSION"
+python3 --version
+echo "PIP INSTALL STARTED"
+python3.9 -m pip install --upgrade pip
+echo "PIP INSTALL ENDED"
+echo "PACKAGES INSTALL STARTED"
 python3.9  -m pip install -r requirements.txt
+echo "PACKAGES INSTALL ENDED"
 python3.9 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
